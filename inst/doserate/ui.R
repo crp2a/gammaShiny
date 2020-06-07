e16e1754806fecb32dd1fc1f239952088dc28bd1
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2020-06-07 15:51:57 +0200

    Add a footer

--- inst/doserate/ui.R
+++ inst/doserate/ui.R
@@ -11,7 +11,14 @@ shiny_ui <- function(request) {
       module_energy_ui("energy"),
       module_dose_ui("dose"),
       module_settings_ui("settings"),
-      module_about_ui("about")
+      module_about_ui("about"),
+      footer = tags$footer(
+        style = "width: 100%;text-align: center;",
+        icon("github"),
+        a(href = "https://github.com/crp2a/gammaShiny/issues",
+          rel = "external", title = "Issue",
+          "Report a bug or request.")
+      )
     )
   )
 }
